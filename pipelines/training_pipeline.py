547519f639d75563c01c0361bc7411c8390fb460
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-01-24 21:51:41 +0100

    Added handle of missing values

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,6 +1,7 @@
 from steps.data_ingestion_step import data_ingestion_step
+from steps.handle_missing_values_step import handle_missing_values_step
 from pyprojroot import here
-from zenml import Model, pipeline, step
+from zenml import Model, pipeline
 
 
 @pipeline(
@@ -12,3 +13,6 @@ def ml_pipeline():
     """Define an end-to-end machine learning pipeline."""
     # Data Ingestion Step
     raw_data = data_ingestion_step(file_path=here("data/archive.zip"))
+
+    # Handling Missing Values Step
+    filled_dara = handle_missing_values_step(raw_data)

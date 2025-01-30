bb590ca8ede073a64ed956f3a6168162b10ab38b
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-01-30 18:11:47 +0100

    Added display of command to run mlflow

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,6 @@
 import click
 from pipelines.training_pipeline import ml_pipeline
+from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
 
 
 @click.command()
@@ -9,6 +10,13 @@ def main():
     """
     # Run the pipeline
     ml_pipeline()
+
+    print(
+        "Now run \n "
+        f"    mlflow ui --backend-store-uri '{get_tracking_uri()}'\n"
+        "To inspect your experiment runs within the mlflow UI.\n"
+        "You can find your runs tracked within the experiment."
+    )
 
 
 if __name__ == "__main__":
